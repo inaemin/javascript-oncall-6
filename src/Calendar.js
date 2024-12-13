df602187fd8f1fbb4ccb64aa554187543ab0f54a
inaemin <inaemin@gmail.com>
2024-12-13 23:20:33 +0900

    feat: Calendar 클래스 구현

---
+++ src/Calendar.js
@@ -0,0 +1,33 @@
+import Date from "./Date.js";
+import { NUM_OF_DATES_IN_MONTH, DAY_OF_WEEK } from "./constants.js";
+
+class Calendar {
+  #calendar = [];
+
+  constructor(month, firstDay) {
+    const firstDayNum = this.#findFirstDayNumber(firstDay);
+    this.#makeCalendar(month, firstDayNum);
+  }
+
+  #makeCalendar(month, firstDayNum) {
+    let day = firstDayNum;
+    for (let date = 1; date <= NUM_OF_DATES_IN_MONTH[month]; date++) {
+      const newDate = new Date(month, date, day);
+      day = (day + 1) % 7;
+      this.#calendar.push(newDate);
+    }
+  }
+
+  #findFirstDayNumber(firstDay) {
+    const keys = Object.keys(DAY_OF_WEEK);
+    for (let i = 0; i < keys.length; i++) {
+      const dayNum = keys[i];
+      const dayValue = DAY_OF_WEEK[dayNum];
+      if (dayValue === firstDay) {
+        return dayNum;
+      }
+    }
+  }
+}
+
+export default Calendar;
