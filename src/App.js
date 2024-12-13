2127f768bf44756138ec6581975997531817130b
inaemin <inaemin@gmail.com>
2024-12-14 01:22:27 +0900

    feat: App 클래스의 run 메소드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import InputManager from "./InputManager.js";
+import AssignManager from "./AssignManager.js";
+import Calendar from "./Calendar.js";
+import OutputManager from "./OutputManager.js";
+
 class App {
-  async run() {}
+  #calendarManager;
+  #assignManager;
+
+  async run() {
+    const [month, firstDay] = await InputManager.readOncallMonthAndStartDay();
+    this.#calendarManager = new Calendar(month, firstDay);
+    const [weekday, weekend] = await InputManager.readOncallMembers();
+    this.#assignManager = new AssignManager(
+      this.#calendarManager,
+      weekday,
+      weekend
+    );
+    OutputManager.printOncallMemberOnCalendar(this.#calendarManager);
+  }
 }
 
 export default App;
